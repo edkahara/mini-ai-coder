3f1f3c7515d4aaee6c2834ce9345d52cc0bc10ee
edkahara <edward@ganzdigital.com>
2025-07-28 20:22:53 +0300

    store a list of messages

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 from google import genai
 import sys
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -11,8 +12,11 @@ def main():
     client = genai.Client(api_key=api_key)
     if len(sys.argv) == 1:
         sys.exit(1)
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=sys.argv[1])]),
+    ]
     response = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents=sys.argv[1]
+        model='gemini-2.0-flash-001', contents=messages
     )
     print(response.text)
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}")
